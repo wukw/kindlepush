716f8d53c9dab5d2f1c0e6949548b95cb5eae61d
wukw <522310157@qq.com>
2018-04-02 10:28:21 +0800

    修改打包问题

--- kindle-search/src/main/java/com/wukw/kindle/Dao/BookDao.java
+++ kindle-search/src/main/java/com/wukw/kindle/Dao/BookDao.java
@@ -3,9 +3,19 @@ package com.wukw.kindle.Dao;
 import com.wukw.kindle.component.BaseDao;
 import com.wukw.kindle.repo.model.Book;
 import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Repository
 public class BookDao extends BaseDao<Book> {
+
+    public List<Book> selectBookPage (String key,Integer page,Integer pageSize){
+        Example example =new Example(Book.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("name",key);
+        return  this.selectPageByExample(page,pageSize,example).getResult();
+    }
 
 
 }
